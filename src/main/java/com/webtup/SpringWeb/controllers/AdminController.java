d1b5990eb08ad61dd23026f8429292e0f846af76
Diego Nunes <nunesdiegond@gmail.com>
2024-05-21 13:42:25 -0300

    criado formulario de cadastro e inserindo informações no banco de dados

--- src/main/java/com/webtup/SpringWeb/controllers/AdminController.java
+++ src/main/java/com/webtup/SpringWeb/controllers/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.webtup.SpringWeb.models.Administrador;
 import com.webtup.SpringWeb.repositorio.AdministradoresRepo;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
@@ -19,12 +20,18 @@ public class AdminController {
     @GetMapping("/admin")
     public String index(Model model) {
         List<Administrador> administradores = (List<Administrador>)repo.findAll();
-        if (administradores.isEmpty()) {
-            System.out.println("Nenhum administrador encontrado.");
-        } else {
-            System.out.println("Administradores carregados: " + administradores.size());
-        }
         model.addAttribute("admin", administradores);
         return "admin/index";
+    }
+
+    @GetMapping("admin/novo")
+    public String novo() {
+        return "admin/novo";
+    }
+
+    @PostMapping("admin/criar")
+    public String criar(Administrador administrador) {
+        repo.save(administrador);
+        return "redirect:/admin";
     }
 }
