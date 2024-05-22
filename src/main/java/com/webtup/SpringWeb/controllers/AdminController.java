c2f2d9072fb7ae6f0e77c7eee844e1ed4eb0c0c7
Diego Nunes <nunesdiegond@gmail.com>
2024-05-22 16:54:01 -0300

    mecanismo de alteracao-atualizacao criado e funcional

--- src/main/java/com/webtup/SpringWeb/controllers/AdminController.java
+++ src/main/java/com/webtup/SpringWeb/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.webtup.SpringWeb.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.webtup.SpringWeb.models.Administrador;
 import com.webtup.SpringWeb.repositorio.AdministradoresRepo;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 public class AdminController {
@@ -33,6 +33,20 @@ public class AdminController {
 
     @PostMapping("admin/criar")
     public String criar(Administrador administrador) {
+        repo.save(administrador);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/atualizaAdmin/{id}")
+    public String busca(@PathVariable int id, Model model) {
+        Optional<Administrador> adm = repo.findById(id);
+        model.addAttribute("administrador", adm.get());
+        return "/admin/atualizaAdmin";
+    }
+
+    @PostMapping("/atualizaAdmin/{id}/atualizar")
+    public String alterar(@PathVariable int id, Administrador administrador) {
+        administrador.setId(id);
         repo.save(administrador);
         return "redirect:/admin";
     }
